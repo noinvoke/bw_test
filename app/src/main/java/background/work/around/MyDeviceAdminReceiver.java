9adc48a4a60b9f001d5eee82763726ef21b6e978
confidenseuide <252536920+confidenseuide@users.noreply.github.com>
2026-06-17 16:07:24 +0500

    Update MyDeviceAdminReceiver.java

--- app/src/main/java/background/work/around/MyDeviceAdminReceiver.java
+++ app/src/main/java/background/work/around/MyDeviceAdminReceiver.java
@@ -34,9 +34,8 @@ public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
                 Intent serviceIntent = new Intent(appContext, NotificationService.class);                
                 appContext.bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE | Context.BIND_IMPORTANT | Context.BIND_ABOVE_CLIENT);
                 try {
-                Class<?> serviceClass = Class.forName("background.work.around.RiderService");
-                Intent serviceIntent2 = new Intent(appContext, serviceClass);
                 appContext.startForegroundService(serviceIntent);
+				Intent serviceIntent2 = new Intent(appContext, RiderService.class);                                	
                 appContext.startForegroundService(serviceIntent2);
                 } catch (Throwable t) {}
                 android.os.SystemClock.sleep(30_000);
