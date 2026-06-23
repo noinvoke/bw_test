f39a6e68582c7ca4ac5de0894251ac8c07e3f6f7
confidenseuide <252536920+confidenseuide@users.noreply.github.com>
2026-06-24 00:54:10 +0500

    Update WhitelistActivity.java

--- app/src/main/java/background/work/around/WhitelistActivity.java
+++ app/src/main/java/background/work/around/WhitelistActivity.java
@@ -10,10 +10,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.ServiceInfo;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.*;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -29,6 +26,19 @@ public class WhitelistActivity extends Activity {
 
     private boolean isEn() {
         return !Locale.getDefault().getLanguage().equals("ru");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
     @Override
@@ -79,6 +89,7 @@ public class WhitelistActivity extends Activity {
             .setTitle(isEn() ? "Alert" : "Внимание")
             .setMessage(isEn() ? msgEn : msgRu)
             .setPositiveButton("OK", null)
+            .setCancelable(false)
             .create();
     
     dialog.show();
