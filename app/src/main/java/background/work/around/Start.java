82ca42b4f51d6930c04b5263b80b0772a1878ef9
noinvoke <noinvoke@gmail.com>
2026-06-29 22:12:27 +0500

    Update Start.java

--- app/src/main/java/background/work/around/Start.java
+++ app/src/main/java/background/work/around/Start.java
@@ -13,13 +13,4 @@ public final class Start {
         } catch(Throwable t) {}
     }    
 
-    public static void RunService2(Context context) {
-        try{          
-        Intent intent = new Intent("background.work.around.START2");
-        intent.setPackage(context.getPackageName());            
-        context.sendBroadcast(intent); 
-        context.sendOrderedBroadcast(intent, null);    
-        } catch(Throwable t) {}
-    }    
-    
 }
